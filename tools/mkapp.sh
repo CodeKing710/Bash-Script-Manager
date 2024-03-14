ddb9174c329fa0f3c524ebd1ec0ef495268d1105
CodeKing710 <cjrox01@yahoo.com>
2024-03-14 11:10:18 -0500

    stubbed mkapp tool

--- tools/mkapp.sh
+++ tools/mkapp.sh
@@ -9,12 +9,45 @@
 #  - 
 
 # Funcs and vars
+__help=`cat <<-HELP
+Usage: mkapp [-h|--help|-v|--version] <-t|---type> <bash|node|python> [-n|--name] <appname> [-l|--location] <applocation>
+HELP
+`
 s_appname=""
 s_apptype=""
+s_apploc=""
+s_usercwd=`pwd`
+
+#__settype() {}
+#__setname() {}
+#__setloc() {}
+
+__args() {
+  while [[ "$1" =~ "-" ]]; do
+    case $1 in
+      -t | --type )
+        shift; __settype "$1"
+        ;;
+      -n | --name )
+        shift; __setname "$1"
+        ;;
+      -l | --location )
+        shift; __setloc "$1"
+        ;;
+      -h | --help )
+        echo "$__help"
+        ;;
+      -v | --version )
+        echo "$__version"
+        ;;
+    esac
+  done
+}
 
 # Main container
 __mkapp() {
-  local args="$@"
+  # Set prog states
+  __args $@
 }
 
 # Run container
